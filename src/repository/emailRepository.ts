27759ce0b4db2d69a0d3f65b3d0a43190a83eb08
Josh <mrjoshc@gmail.com>
2025-04-10 17:47:29 -0500

    Cleanup

--- src/repository/emailRepository.ts
+++ src/repository/emailRepository.ts
@@ -1,19 +1,3 @@
-/*
-Fields the table should have:
-- id (primary key)
-- to (text)
-- subject (text)
-- body (text)
-- sent (boolean)
-- sent_at (timestamp)
-- created_at (timestamp)
-- updated_at (timestamp)
-
-methods need:
-- get emails
-- save email
- */
-
 import {Email} from "../types/email.ts";
 import {Client} from "pg";
 
